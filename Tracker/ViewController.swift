e127abcfeffaaf1a4bc8cb75ff8894fee31387db
bakytushka <alekzandr14@gmail.com>
2024-06-30 15:28:51 +0200

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by Bakyt Temishov on 30.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

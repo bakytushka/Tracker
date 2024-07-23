882532eab23c8c895803738423fc5a6e1dacb954
bakytushka <alekzandr14@gmail.com>
2024-07-24 01:20:58 +0200

    Now the tab bar has a gray border on the main screen

--- Tracker/TabBarController/TabBarController.swift
+++ Tracker/TabBarController/TabBarController.swift
@@ -12,6 +12,7 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createTabBarController()
+        addTabBarBorder()
     }
     
     private func createTabBarController() {
@@ -29,5 +30,19 @@ final class TabBarController: UITabBarController {
             selectedImage: nil
         )
         viewControllers = [trackersViewController, statisticViewController]
+    }
+    
+    func addTabBarBorder(){
+        let border = UIView()
+        border.backgroundColor = UIColor(red: 174.0 / 255.0, green: 175.0 / 255.0, blue: 180.0 / 255.0, alpha: 1.0)
+        border.translatesAutoresizingMaskIntoConstraints = false
+        tabBar.addSubview(border)
+        
+        NSLayoutConstraint.activate([
+            border.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor),
+            border.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor),
+            border.topAnchor.constraint(equalTo: tabBar.topAnchor),
+            border.heightAnchor.constraint(equalToConstant: 1)
+        ])
     }
 }

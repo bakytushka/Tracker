65d1161671c0d73ecde2be6203dae983a56e635d
bakytushka <alekzandr14@gmail.com>
2024-07-29 04:12:37 +0200

    Created Persistent Container, initialized the stack in AppDelegate, created TrackerStore, TrackerCategoryStore, TrackerRecordStore classes

---
+++ Tracker/Domain/TrackerCategoryStore.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerCategoryStore.swift
+//  Tracker
+//
+//  Created by Bakyt Temishov on 29.07.2024.
+//
+
+import Foundation
+import CoreData
+
+final class TrackerCategoryStore {
+    
+}
---
+++ Tracker/Domain/TrackerStore.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerStore.swift
+//  Tracker
+//
+//  Created by Bakyt Temishov on 29.07.2024.
+//
+
+import Foundation
+import CoreData
+
+final class TrackerStore {
+    
+}

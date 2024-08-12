dc00f4e59fdb6123b0234a82b2b2225e2bd101e5
bakytushka <alekzandr14@gmail.com>
2024-08-12 04:35:02 +0200

    Implemented Onboarding for the application

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -12,10 +12,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = TabBarController()
+        
+        let hasSeenOnboarding = UserDefaults.standard.bool(forKey: "hasSeenOnboarding")
+        
+        if hasSeenOnboarding {
+            window?.rootViewController = TabBarController()
+        } else {
+            let onboardingViewController = OnboardingViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+            onboardingViewController.completionHandler = {
+                UserDefaults.standard.set(true, forKey: "hasSeenOnboarding")
+                UserDefaults.standard.synchronize()
+                self.window?.rootViewController = TabBarController()
+            }
+            window?.rootViewController = onboardingViewController
+        }
+        
         window?.makeKeyAndVisible()
     }
     
